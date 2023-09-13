6414284baee8aaaf8205c9cef0ab0ab7840db192
Welley <monteirowelley7@gmail.com>
2023-09-13 09:10:23 -0300

    tratand erros

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, ':', 'Repositorio não encontrado'));
+}
+
 function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
 
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
-        console.log(chalk.green(texto))
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+
+        if (erro) {
+            trataErro(erro);
+        }
+
+        console.log(chalk.green(texto));
     });
 };
 
-pegaArquivo('./arquivos/texto.md')
+pegaArquivo('./arquivos/texto.md');
