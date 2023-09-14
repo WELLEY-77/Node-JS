e203c51920f6cfcc764d733a510dfb51900b1d48
Welley <monteirowelley7@gmail.com>
2023-09-14 09:37:06 -0300

    regex

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
+function extrairlinks(texto) {
+
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)]
+    const resultados = capturas.map(resultado => ({[resultado[1]]:resultado[2]}))
+    return resultados
+}
+
 function trataErro(erro) {
     throw new Error(chalk.red(erro.code, ':', 'Repositorio não encontrado'));
 }
@@ -12,23 +20,11 @@ async function pegaArquivo(caminhoDoArquivo) {
     try {
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-    
-        console.log(chalk.green(texto))
+        console.log(extrairlinks(texto))
     } catch(erro) {
         trataErro(erro)
     }
 
 }
-
-
-// Promises
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'utf-8';
-
-//     fs.promises.readFile(caminhoDoArquivo, encoding)
-//       .then((texto) => console.log(chalk.green(texto)))
-//       .catch(trataErro);
-// }
-
 
 pegaArquivo('./arquivos/texto.md');
