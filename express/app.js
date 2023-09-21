65f484b5f6178bc800160415385036e86727c3d6
Welley <monteirowelley7@gmail.com>
2023-09-21 09:11:00 -0300

    criando a primeira rota com express

--- express/app.js
+++ express/app.js
@@ -1,1 +1,11 @@
-import { Express } from "express";
+import express from 'express'
+
+const app = express();
+
+app.get('', (req, res) => {
+    res.send('<h1> Express </h1>')
+})
+
+app.listen(3000, () => {
+    console.log('Servidor ativo')
+})
