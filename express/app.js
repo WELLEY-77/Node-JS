aa6b730a6c8bb6b95415edb88fa00e19dc04a061
Welley <monteirowelley7@gmail.com>
2023-09-21 09:16:15 -0300

    respostas json

--- express/app.js
+++ express/app.js
@@ -2,8 +2,12 @@ import express from 'express'
 
 const app = express();
 
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
     res.send('<h1> Express </h1>')
+})
+
+app.get('/json', (req, res) => {
+    res.json({title:'Space X', done:true})
 })
 
 app.listen(3000, () => {
